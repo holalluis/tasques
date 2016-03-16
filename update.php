115c48f7c2bbb2e3006504f08efbc87b3e8e853a
lbosch <lbosch@icra.cat>
2016-03-16 16:40:47 +0100

    update modified to show output correctly, with error reporting

--- update.php
+++ update.php
@@ -2,9 +2,29 @@
 /*
 	run "git pull" to update the app
 */
-//build shell command
-$command='"C:\Program Files (x86)\Git\bin"\git pull';
-echo "<h2>Executing: '$command'</h2>";
+
+	//the command (windows)
+	$command='"C:\Program Files (x86)\Git\bin"\git pull';
+
+	//show command
+	echo "<h2>Executing: '".str_replace('"','',$command)."'</h2>";
 ?>
-<hr><div><code><?php passthru($command)?></code></div>
+
+<hr><div><code>
+<?php 
+	//execute command
+	$result=exec($command);
+
+	//show output
+	echo $result;
+
+	//show warning if command fails
+	if($result=="")
+	{
+		?>
+		<h4>Error. Make sure you have <a href=https://git-scm.com/>git</a> installed in your computer</h4>
+		<?php
+	}
+?></code></div><hr>
+
 <button onclick="window.location='index.php'">Go to main page</button>
